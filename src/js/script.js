8b00aee0288d4e711cf6e2d4487af6cd281ec01f
louis <quocbinhgithub@gmail.com>
2023-07-09 11:12:38 +0700

    feat: shadow

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"; // mo
 import * as datGUI from "dat.gui"; //giao diện đồ học đơn giản
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true; // cho phép đổ bóng
 document.body.appendChild(renderer.domElement);
 
 // tao camera
@@ -26,23 +27,6 @@ orbit.update(); //Cập nhập lại khung hình khi camera thay đổi quỹ đ
 // camera.position.y = 2; // đặt vị trí của camera theo trục Y
 camera.position.set(-10, 30, 30);
 
-//Tạo 1 BOX (vật thể hình hộp)
-const BoxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const BoxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // Đặt vật liệu cho BOX
-const cube = new THREE.Mesh(BoxGeometry, BoxMaterial);
-scene.add(cube);
-
-//Tạo 1 quả cầu
-const sphereGeometry = new THREE.SphereGeometry(4, 64, 32);
-const sphereMaterial = new THREE.MeshStandardMaterial({
-  color: "#049ef4",
-  wireframe: true, //wireframe là hiện khung xương, ta sẽ thấy các vật thể bên trong
-});
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-sphere.position.set(10, 10, 0);
-
-scene.add(sphere);
-
 //config GUI
 const gui = new datGUI.GUI();
 const options = {
@@ -59,6 +43,23 @@ gui.add(options, "wireframe").onChange(function (e) {
 });
 gui.add(options, "speed", 0, 0.1);
 
+//Tạo 1 BOX (vật thể hình hộp)
+const BoxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const BoxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // Đặt vật liệu cho BOX
+const cube = new THREE.Mesh(BoxGeometry, BoxMaterial);
+scene.add(cube);
+
+//Tạo 1 quả cầu
+const sphereGeometry = new THREE.SphereGeometry(4, 64, 32);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  color: "#049ef4",
+  wireframe: true, //wireframe là hiện khung xương, ta sẽ thấy các vật thể bên trong
+});
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.set(3, 10, 0);
+sphere.castShadow = true; //Hình cấu là nguồn tạo bóng xuống mặt phẳng
+scene.add(sphere);
+
 //Tạo 1 mặt phẳng
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
 const planeMaterial = new THREE.MeshStandardMaterial({
@@ -67,6 +68,8 @@ const planeMaterial = new THREE.MeshStandardMaterial({
 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -0.5 * Math.PI; // đặt mặt phảng nằm ngang đúng vị trí
+
+plane.receiveShadow = true; //Mặt phẳng là nơi nhận bóng(shadow) từ hình cầu
 scene.add(plane);
 
 //Taọ lưới hỗ trợ đo đạc
@@ -84,19 +87,26 @@ cube.rotation.y = 6;
 const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
 //Xác định hướng màu và độ tương phản của ánh sáng
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
 directionalLight.position.set(-30, 40, 0);
-
+directionalLight.castShadow = true; //nguồn sáng chiếu xuống sphere, sphere đổ bóng xuống mặt phẳng
+directionalLight.shadow.camera.top = 12; //băng kích thước vùng đổ bóng của máy ảnh
 scene.add(directionalLight);
 //Khởi tạo đối tượng nguồn sáng, ta có thể duy chuyển nguồn sáng để tạo bóng, phải có đối tương Helper mới có thể điều chỉnh vị trí nguồn sáng được(directionalLight.position.set(-30, 40, 0);)
 const directionalLightHelper = new THREE.DirectionalLightHelper(
-  directionalLight
+  directionalLight,
+  5
 );
 scene.add(directionalLightHelper);
 
+//Camera hỗ trợ đỗ bóng, helper naỳ bẽ cho ta các đường sáng , giúp điều chỉnh ánh sáng trực quang hơn
+const directionalLightCameraShadow = new THREE.CameraHelper(
+  directionalLight.shadow.camera
+);
+scene.add(directionalLightCameraShadow);
+
 //tạo hành động nhảy lên cho sphere(khối cầu)
 let step = 0;
-
 //Tạo 1 hàm xoay theo thời gian
 function animation(time) {
   cube.rotation.x = time / 1000; // xoay theo thời gian
